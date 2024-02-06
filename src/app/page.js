5de272daa990c528d10a98cb25f3e01bf3d17b34
Mehar Ali <mehar0alii@gmail.com>
2024-02-06 12:21:49 +0500

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import BarCodeInfo from "@/components/BarCodeInfo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-5">
       <BarCodeInfo />
     </main>
   );
--- src/components/BarCodeInfo.jsx
+++ src/components/BarCodeInfo.jsx
@@ -1,8 +1,8 @@
 "use client";
 import axios from "axios";
-import { FiSearch } from 'react-icons/fi';
-import { BiErrorCircle } from 'react-icons/bi';
-import { motion } from 'framer-motion';
+import { FiSearch } from "react-icons/fi";
+import { BiErrorCircle } from "react-icons/bi";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React, { useState } from "react";
 
@@ -22,7 +22,6 @@ const BarCodeInfo = () => {
       );
       setProduct(res.data.product);
       setError(null); // Clear any previous errors
-      setBarcode('')
     } catch (error) {
       setError("Error getting data. Please try again.");
       console.log("Error getting data: ", error);
@@ -30,31 +29,33 @@ const BarCodeInfo = () => {
   };
 
   return (
-    <motion.main 
-      className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 p-5 bg-gray-900 rounded-2xl mx-auto scrollbar-hide"
+    <motion.main
+      className="w-full sm:w-5/6 md:w-4/5 lg:w-3/5 xl:w-2/3 p-5 rounded-2xl mx-auto"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
       <div className="w-full flex justify-center items-center flex-col space-y-4">
-        <label htmlFor="text" className="text-white font-semibold text-lg">
-          Enter Product Barcode:
-        </label>
-        <div className="flex items-center space-x-2">
-          <input
-            type="number"
-            id="text"
-            value={barcode}
-            onChange={handleChange}
-            className="w-full sm:w-auto outline-none ring-2 focus:ring-4 text-black rounded-xl px-3 py-1"
-            placeholder="Enter the Barcode"
-          />
-          <button
-            onClick={getData}
-            className="p-3 bg-blue-500 hover:bg-blue-600 rounded-full text-white font-semibold flex items-center space-x-2"
-          >
-            <FiSearch />
-          </button>
+        <div className="bg-gray-900 px-5 py-5 rounded-xl">
+          <label htmlFor="text" className="text-white font-semibold text-lg">
+            Enter Product Barcode:
+          </label>
+          <div className="flex items-center space-x-2">
+            <input
+              type="number"
+              id="text"
+              value={barcode}
+              onChange={handleChange}
+              className="w-full sm:w-auto outline-none ring-2 focus:ring-4 text-black rounded-xl px-3 py-1"
+              placeholder="Enter the Barcode"
+            />
+            <button
+              onClick={getData}
+              className="p-3 bg-blue-500 hover:bg-blue-600 rounded-full text-white font-semibold flex items-center space-x-2"
+            >
+              <FiSearch />
+            </button>
+          </div>
         </div>
         {error && (
           <div className="flex items-center space-x-2 bg-red-500 text-white rounded-lg p-2">
@@ -63,44 +64,64 @@ const BarCodeInfo = () => {
           </div>
         )}
         {product && (
-          <motion.div 
-            className="w-full flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4"
+          <motion.div
+            className="w-full flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 bg-gray-900 p-5 rounded-xl"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.5 }}
           >
-            <motion.div 
-              className="w-full sm:w-1/2 flex justify-center items-center"
+            <motion.div
+              className="w-full sm:w-1/3 flex justify-center items-center"
               initial={{ opacity: 0, x: -20 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ delay: 0.5, duration: 0.5 }}
             >
-              <Image
-                src={product.image_url}
-                alt="Product Image"
-                width={400}
-                height={400}
-                className="rounded-xl"
-              />
+              <div className="rounded-xl overflow-hidden">
+                <Image
+                  src={product.image_url}
+                  alt="Product Image"
+                  layout="responsive"
+                  width={500}
+                  height={500}
+                />
+              </div>
             </motion.div>
-            <motion.div 
-              className="w-full sm:w-1/2 flex flex-col justify-center items-center text-white"
+            <motion.div
+              className="w-full sm:w-2/3 flex flex-col justify-center items-center text-white"
               initial={{ opacity: 0, x: 20 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ delay: 0.5, duration: 0.5 }}
             >
-              <h2 className="font-semibold text-xl mb-2">Product Information</h2>
-              <motion.div 
-                className="w-full flex flex-col space-y-4 "
+              <h2 className="font-semibold text-xl mb-2">
+                Product Information
+              </h2>
+              <motion.div
+                className="w-full grid grid-cols-2 gap-4"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.7, duration: 0.5 }}
               >
                 <InfoItem label="Code" value={product.code} />
                 <InfoItem label="Name" value={product.product_name} />
-                <InfoItem label="Country of Origin" value={product.countries_tags} />
-                <InfoItem label="Energy" value={`${product.nutriments?.energy} kg`} />
+                <InfoItem
+                  label="Country of Origin"
+                  value={product.countries_tags}
+                />
+                <InfoItem
+                  label="Energy"
+                  value={`${product.nutriments?.energy} kg`}
+                />
                 <InfoItem label="Brand Owner" value={product.brand_owner} />
+                <InfoItem
+                  label="Ingredients"
+                  value={product.ingredients_text}
+                />
+                <InfoItem label="Allergens" value={product.allergens} />
+                <InfoItem
+                  label="Nutrition Grade"
+                  value={product.nutrition_grade_fr}
+                />
+                <InfoItem label="Quantity" value={product.quantity} />
               </motion.div>
             </motion.div>
           </motion.div>
@@ -111,7 +132,7 @@ const BarCodeInfo = () => {
 };
 
 const InfoItem = ({ label, value }) => (
-  <div className="w-full bg-gray-800 rounded-xl p-4">
+  <div className="bg-gray-800 rounded-xl p-4">
     <p className="text-gray-400">{label}</p>
     <p className="text-white mt-1">{value}</p>
   </div>
