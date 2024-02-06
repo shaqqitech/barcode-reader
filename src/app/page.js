eea15511349c68de6e4a04b025d4ff651e04cd22
Mehar Ali <mehar0alii@gmail.com>
2024-02-06 12:29:48 +0500

    minor change

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import BarCodeInfo from "@/components/BarCodeInfo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-5">
+    <main className="flex min-h-screen flex-col items-center justify-center p-5 bg-black">
       <BarCodeInfo />
     </main>
   );
